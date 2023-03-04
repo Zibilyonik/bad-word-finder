07a6714dc5f68c9339111eb006b8b8bf0d42c65d
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-03-04 21:27:12 +0100

    added word search function, commented out old code

--- bad-word-finder.go
+++ bad-word-finder.go
@@ -5,22 +5,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	var file_name string
+	var file_name, word string
 	fmt.Scan(&file_name)
 	file, err := os.Open(file_name)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	fmt.Scan(&word)
 	scanner := bufio.NewScanner(file)
 
+	// first step of the project
+	// for scanner.Scan() {
+	// 	fmt.Println(scanner.Text())
+	// }
+
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		if strings.ToLower(scanner.Text()) == strings.ToLower(word) {
+			fmt.Println("true")
+			return
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("false")
 }
